8350d5f58b2d8125f99fa152fcda4ae1a015613b
Edsel Serrano <eserrano@leadtech.com>
2024-05-25 15:54:14 +0200

    chore: update deps to react 19

--- src/services/races.ts
+++ src/services/races.ts
@@ -2,7 +2,12 @@ export const getRaces = async () => {
   const API_ENDPOINT = process.env.API_ENDPOINT;
   const SERVICE = "sessions";
   const QUERIES = "?year=2024";
-  const response = await fetch(API_ENDPOINT + SERVICE + QUERIES);
-  const racesData = await response.json();
-  return racesData;
+  try {
+    const response = await fetch(API_ENDPOINT + SERVICE + QUERIES);
+    const racesData = await response.json();
+    return racesData;
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+  return [];
 };
