097dc2047f91f76090a08ea8566eef7e951f6301
Edsel Serrano <edsel@zabvio.com>
2025-06-04 18:57:25 +0200

    fix pitstops data population

--- src/services/pitstops.ts
+++ src/services/pitstops.ts
@@ -26,7 +26,7 @@ export const getPitstops = async (sessionKey: string) => {
 
     const response = await fetch(API_ENDPOINT + SERVICE + QUERIES);
 
-    const raceControlData = await response.json();
+    raceControlData = await response.json();
     for (let i = 0; i < raceControlData.length; i++) {
       const driver = await getDriver(raceControlData[i].driver_number);
       raceControlData[i] = { ...raceControlData[i], driver };
